d488f240b366205bd4488ea3a11c1c90460d5b8d
faisalantu <faisalantu@gmail.com>
2022-05-22 04:33:20 +0600

    🦁 Review Schema done

---
+++ models/Review.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const ReviewSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  userDisplayName: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Review", ReviewSchema);
