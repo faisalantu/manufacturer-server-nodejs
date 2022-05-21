29fc11567cea12743c300c88e78000808eaecedd
faisalantu <faisalantu@gmail.com>
2022-05-22 04:07:06 +0600

    🐕‍🦺 initial server done

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+
+const app = express();
+app.use(cors());
+
+//connect database
+connectDB();
+// init middleware
+app.use(express.json({ limit: "30mb" }));
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "hello world!",
+  });
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}!`);
+});
