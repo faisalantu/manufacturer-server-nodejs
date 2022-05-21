8f20d5c869a732fab09d3036d256b67dd107887f
faisalantu <faisalantu@gmail.com>
2022-05-22 04:28:51 +0600

    🔐 auth middleware added

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  const token = req.header("authHeader");
+
+  if (!token) {
+    return res.status(401).json({ msg: "no token, auth denied 401" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+
+    console.log(decoded);
+    next();
+  } catch (err) {
+    res.status(403).json({ msg: "token is not valid forbiden 403" });
+  }
+};
