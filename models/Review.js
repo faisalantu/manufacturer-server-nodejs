4cabc679772a97e5a05a4a04669e385e7a42c320
faisalantu <faisalantu@gmail.com>
2022-05-25 02:14:12 +0600

    🍟 review model updated

--- models/Review.js
+++ models/Review.js
@@ -13,8 +13,12 @@ const ReviewSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  userDisplayName: {
+  displayName: {
     type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
     required: true,
   },
   date: {
